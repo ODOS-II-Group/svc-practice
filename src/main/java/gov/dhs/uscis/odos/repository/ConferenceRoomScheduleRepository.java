5e6173063ec67d7ac1631412be7d739233b4610a
pChou19 <pchou19@gmail.com>
2018-07-21 11:19:56 -0400

    Add endpoint for checking if conf room is available

--- src/main/java/gov/dhs/uscis/odos/repository/ConferenceRoomScheduleRepository.java
+++ src/main/java/gov/dhs/uscis/odos/repository/ConferenceRoomScheduleRepository.java
@@ -1,12 +1,14 @@
 package gov.dhs.uscis.odos.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import gov.dhs.uscis.odos.domain.ConferenceRoomSchedule;
-
 
 /**
  * Spring Data JPA repository for the ConferenceRoomSchedule entity.
@@ -15,4 +17,10 @@ import gov.dhs.uscis.odos.domain.ConferenceRoomSchedule;
 @Repository
 public interface ConferenceRoomScheduleRepository extends JpaRepository<ConferenceRoomSchedule, Long> {
 	public List<ConferenceRoomSchedule> findByRequestorId(String requestorId);
+
+	@Query("SELECT cs FROM ConferenceRoomSchedule cs inner join cs.conferenceRoom cf where cf.conferenceRoomId = :conferenceRoomId and "
+			+ " cs.roomScheduleEndTime >= :startDate AND cs.roomScheduleEndTime <= :endDate")
+	public List<ConferenceRoomSchedule> findAllByConferenceRoomIdAndDate(
+			@Param("conferenceRoomId") Long conferenceRoomId, @Param("startDate") Date startDate,
+			@Param("endDate") Date endDate);
 }
