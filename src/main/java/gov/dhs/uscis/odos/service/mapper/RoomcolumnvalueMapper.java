10abb7bf851a070ba11f2bd7be66d9779eda5df2
pChou19 <pchou19@gmail.com>
2018-08-22 19:03:26 -0400

    ODOS_5: Mapper for Roomcolumnvalue

--- src/main/java/gov/dhs/uscis/odos/service/mapper/RoomcolumnvalueMapper.java
+++ src/main/java/gov/dhs/uscis/odos/service/mapper/RoomcolumnvalueMapper.java
@@ -1,24 +1,51 @@
 package gov.dhs.uscis.odos.service.mapper;
 
-import gov.dhs.uscis.odos.domain.*;
-import gov.dhs.uscis.odos.service.dto.RoomcolumnvalueDTO;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.mapstruct.*;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.dozer.Mapper;
+
+import gov.dhs.uscis.odos.domain.Roomcolumnvalue;
+import gov.dhs.uscis.odos.service.dto.RoomcolumnvalueDTO;
 
 /**
  * Mapper for the entity Roomcolumnvalue and its DTO RoomcolumnvalueDTO.
  */
-@Mapper(componentModel = "spring", uses = {})
-public interface RoomcolumnvalueMapper extends EntityMapper<RoomcolumnvalueDTO, Roomcolumnvalue> {
+@Named
+public class RoomcolumnvalueMapper implements EntityMapper<RoomcolumnvalueDTO, Roomcolumnvalue> {
 
+	@Inject
+	private Mapper mapper;
 
+	@Override
+	public Roomcolumnvalue toEntity(RoomcolumnvalueDTO dto) {
+		return mapper.map(dto, Roomcolumnvalue.class);
+	}
 
-    default Roomcolumnvalue fromId(Long id) {
-        if (id == null) {
-            return null;
-        }
-        Roomcolumnvalue roomcolumnvalue = new Roomcolumnvalue();
-        roomcolumnvalue.setId(id);
-        return roomcolumnvalue;
-    }
+	@Override
+	public RoomcolumnvalueDTO toDto(Roomcolumnvalue entity) {
+		RoomcolumnvalueDTO roomcolumnvalueDTO = mapper.map(entity, RoomcolumnvalueDTO.class);
+		return roomcolumnvalueDTO;
+	}
+
+	@Override
+	public List<Roomcolumnvalue> toEntity(List<RoomcolumnvalueDTO> dtoList) {
+		List<Roomcolumnvalue> roomcolumnvalues = new ArrayList<>();
+		for (RoomcolumnvalueDTO roomcolumnvalueDTO : dtoList) {
+			roomcolumnvalues.add(mapper.map(roomcolumnvalueDTO, Roomcolumnvalue.class));
+		}
+		return roomcolumnvalues;
+	}
+
+	@Override
+	public List<RoomcolumnvalueDTO> toDto(List<Roomcolumnvalue> entityList) {
+		List<RoomcolumnvalueDTO> roomcolumnvalueDTO = new ArrayList<>();
+		for (Roomcolumnvalue roomcolumnvalue : entityList) {
+			roomcolumnvalueDTO.add(mapper.map(roomcolumnvalue, RoomcolumnvalueDTO.class));
+		}
+		return roomcolumnvalueDTO;
+	}
 }
